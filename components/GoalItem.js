ca30c1945f85ae05b42f10c2c9d27f50a92eb465
Mathew Chong <mathewchong.dev@gmail.com>
2025-01-16 06:28:11 +1100

    Press to delete item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 export default function GoalItem(props) {
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    <Pressable onPress={() => props.onDeleteItem(props.id)}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
@@ -19,4 +21,4 @@ const styles = StyleSheet.create({
   goalText: {
     color: "white",
   },
-})
+});
