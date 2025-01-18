fba96b5329495195b2884cad9dac2047d9cdc852
Mathew Chong <mathewchong.dev@gmail.com>
2025-01-19 09:06:51 +1100

    Auto-format

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -23,7 +23,10 @@ function GoalInput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
-        <Image style={styles.image} source={require('../assets/images/goal.png')} />
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -35,7 +38,7 @@ function GoalInput(props) {
             <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0"/>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
         </View>
       </View>
@@ -51,18 +54,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
-    backgroundColor: '#311b6b'
+    backgroundColor: "#311b6b",
   },
   image: {
     width: 100,
     height: 100,
-    margin: 20
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#e4d0ff",
-    backgroundColor: '#e4d0ff',
-    color: '#120438',
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
     borderRadius: 6,
     width: "100%",
     padding: 16,
